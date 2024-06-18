d7dc6ce528a4bb45dd966b1f1fe499d56f039cfa
AsierAlaminos <alaminosasier@gmail.com>
2024-06-18 20:35:38 +0200

    fix: spaces to tabs

--- init.lua
+++ init.lua
@@ -5,3 +5,8 @@ vim.keymap.set("n", "<leader>e", ':NvimTreeFocus<CR>')
 vim.keymap.set("n", "<leader>q", ':NvimTreeClose<CR>')
 vim.keymap.set("n", "<leader>tf", ':NvimTreeFindFile<CR>')
 vim.keymap.set("n", "<leader>f", ':wincmd l<CR>')
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = false
